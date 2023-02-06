b3e5b1ea032faf83a13da799788ea1449687c086
Pablov <juan.vasquez@valposystem.com>
2023-02-05 22:26:01 -0300

    Completar información de clases de paquete ports

--- src/main/java/cl/banchile/bchrest/administra/acciones/domain/ports/in/OperacionBancoPort.java
+++ src/main/java/cl/banchile/bchrest/administra/acciones/domain/ports/in/OperacionBancoPort.java
@@ -1,8 +1,13 @@
 package cl.banchile.bchrest.administra.acciones.domain.ports.in;
 
+
+import cl.banchile.go.ejb.OperacionAccionesTO;
+import cl.banchile.go.ejb.OperacionException_Exception;
+
 /**
  * @author Pablo
  *
  */
 public interface OperacionBancoPort {
+    OperacionAccionesTO addOperacionAccion(OperacionAccionesTO operacion) throws OperacionException_Exception;
 }
--- src/main/java/cl/banchile/bchrest/administra/acciones/domain/ports/out/OperacionBancoEJBPort.java
+++ src/main/java/cl/banchile/bchrest/administra/acciones/domain/ports/out/OperacionBancoEJBPort.java
@@ -1,8 +1,15 @@
 package cl.banchile.bchrest.administra.acciones.domain.ports.out;
 
+import cl.banchile.bchrest.administra.acciones.application.adapters.in.rest.model.OperacionAccionesModel;
+import cl.banchile.go.ejb.OperacionAccionesTO;
+import cl.banchile.go.ejb.OperacionException_Exception;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 /**
  * @author Pablo
  *
  */
+
 public interface OperacionBancoEJBPort {
+    public OperacionAccionesModel addOperacionAcciones(OperacionAccionesTO operacion) throws OperacionException_Exception, JsonProcessingException;
 }
