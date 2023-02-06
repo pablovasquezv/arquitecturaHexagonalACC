88231028f1ce3c1f82c4a389352e1960b09587af
Pablov <juan.vasquez@valposystem.com>
2023-02-05 21:07:33 -0300

    Configuración clasee

---
+++ src/test/java/cl/banchile/bchrest/administra/acciones/AdministraAccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.banchile.bchrest.administra.acciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministraAccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
